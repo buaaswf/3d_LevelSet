93c1ad9967faf8629d4f9b643d06018c71a5e982
buaa.swf <876267233@qq.com>
2013-11-25 15:41:16 +0800

    no need to be initialed
    20131125

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 void testcolon(int argc,char **argv)
 {
 	char *pt="single_well";
-	int l=512,m=512,n=30,l1=0,l2=0,iter_outer=50;
+	int l=512,m=512,n=60,l1=0,l2=0,iter_outer=50;
 	//int LX=0,LY=0,LZ=0;
 	
 	RawImage test;
@@ -77,39 +77,83 @@ void testcolon(int argc,char **argv)
 	//int x1=84,x2=123,y1=118,y2=180,z1=19,z2=24; //test for little data
 
 	//int x1=257,x2=299,y1=345,y2=358,z1=6,z2=16; //test for colon data inside 
-	int x1=225,x2=346,y1=301,y2=398,z1=6,z2=16; //test for colon data outside 
-	for (int i=0;i<l;i++)
-	{
-		for (int j=0;j<m;j++)
-		{
-			for (int k=0;k<n;k++)
-			{
+	//int x1=225,x2=346,y1=301,y2=398,z1=6,z2=16; //test for colon data outside 
+	//	int x1=140,x2=405,y1=269,y2=372,z1=1,z2=30; //test for colon data outside 
+	//	int x12,x22,y12,y22,z12,z22;
+	//	int x13,x23,y13,y23,z13,z23;
+	//	int x14,x24,y14,y24,z14,z24;
+	//for (int i=0;i<l;i++)
+	//{
+	//	for (int j=0;j<m;j++)
+	//	{
+	//		for (int k=0;k<n;k++)
+	//		{
+	//			//region 1
+	//			if (i>x1 && j>y1 && k>z1 && i<x2 && j<y2 && k<z2)
+	//			{
+	//				initial->put(i,j,k,-2.0);
+	//			} 
+	//			else if (i<x1|| j<y1 || k < z1 || i>x2 || j>y2 || k> z2)
+	//			{
+	//				initial->put(i,j,k,2.0);
+	//			}
+	//			else {
+	//				initial->put(i,j,k,-2.0);
+	//			}
+	//			//region 2
+	//			if (i>x12 && j>y12 && k>z12 && i<x22 && j<y22 && k<z22)
+	//			{
+	//				initial->put(i,j,k,-2.0);
+	//			} 
+	//			else if (i<x12|| j<y12 || k < z12 || i>x22 || j>y22 || k> z22)
+	//			{
+	//				initial->put(i,j,k,2.0);
+	//			}
+	//			else {
+	//				initial->put(i,j,k,-2.0);
+	//			}
+	//			//region 3
+	//			if (i>x13 && j>y13 && k>z13 && i<x23 && j<y23 && k<z23)
+	//			{
+	//				initial->put(i,j,k,-2.0);
+	//			} 
+	//			else if (i<x14|| j<y14 || k < z14 || i>x24 || j>y24 || k> z24)
+	//			{
+	//				initial->put(i,j,k,2.0);
+	//			}
+	//			else {
+	//				initial->put(i,j,k,-2.0);
+	//			}
 
-				if (i>x1 && j>y1 && k>z1 && i<x2 && j<y2 && k<z2)
-				{
-					initial->put(i,j,k,-2.0);
-				} 
-				else if (i<x1|| j<y1 || k < z1 || i>x2 || j>y2 || k> z2)
-				{
-					initial->put(i,j,k,2.0);
-				}
-				else {
-					initial->put(i,j,k,-2.0);
-				}
-
-			}
-		}
-	}
+	//		}
+	//	}
+	//}
 	//IShowraw(*initial,argc,argv);
 	RawImage *write=new RawImage();
 
 	ThreeDim_LevelSet *ls=new ThreeDim_LevelSet();
 	//IShowraw(*input,argc,argv);
-	ls->initialg(*input);	
+	ls->initialg(*input);
+for (int i=0; i<input->getXsize(); i++)
+{
+	for (int j=0; j<input->getYsize(); j++)
+	{
+		for (int k=0; k<input->getZsize(); k++)
+		{
+			if (input->get(i,j,k)>=1)
+			{
+				initial->put(i,j,k,-2);
+			}
+			else initial->put(i,j,k,2);
+
+		}
+	}
+}
 	IShowraw(*input,argc,argv);
+	IShowraw(*initial,argc,argv);
 	ls->minimal_surface(*initial,*input,5.0,0.1,-3,1.5,1,iter_outer,pt);
 	IShowraw(*initial,argc,argv);
-	test.writeImage(*input);
+	test.writeImage(*initial);
 
 
 
